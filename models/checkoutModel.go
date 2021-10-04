e8e39c7152867ca8e947867a71576387ba14cb1f
kevinabdul <kevin.abdulfattah1@gmail.com>
2021-10-04 10:29:44 +0700

    minor change on checkout feature

--- models/checkoutModel.go
+++ models/checkoutModel.go
@@ -2,6 +2,6 @@ package models
 
 type CheckoutGetResponse struct {
 	Recipients	 		[]RecipientGift 	`json:"recipients"`
-	PackageList 		[]PackageListAPI 	`json:"package_list"`
-	PaymentOptions 		[]PaymentOption		`json:"payment_options"`
+	PackageList 		[]PackageListAPI 	`json:"package_lists"`
+	PaymentMethods 		[]PaymentMethod		`json:"payment_methods"`
 }
