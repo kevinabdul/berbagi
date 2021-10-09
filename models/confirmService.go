a422aa471f508e2c2f384a539fa9d987b554ecb5
urnikrokhiyah <urnikrokhiyah123@gmail.com>
2021-10-09 12:58:46 +0700

    add verification letter display feature

--- models/confirmService.go
+++ models/confirmService.go
@@ -23,3 +23,14 @@ type ResponseConfirmServices struct {
 	StartDate        time.Time `json:"start_date" form:"start_date"`
 	FinishDate       time.Time `json:"finish_date" form:"finish_date"`
 }
+
+type ResponseVerification struct {
+	Invoice          string `json:"invoice"`
+	VolunteerName    string `json:"volunteer_name"`
+	AddressVolunteer string `json:"volunteer_address"`
+	ProficiencyName  string `json:"proficiency_name"`
+	UserName         string `json:"recipient_name"`
+	AddressUser      string `json:"recipient_address" form:"recipient_address"`
+	StartDate        string `json:"start_date" form:"start_date"`
+	FinishDate       string `json:"finish_date" form:"finish_date"`
+}
