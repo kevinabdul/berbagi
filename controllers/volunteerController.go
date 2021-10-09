71f1f5d7ad514e5f1d86b5309c951a43d16dbd87
urnikrokhiyah <urnikrokhiyah123@gmail.com>
2021-10-09 20:54:12 +0700

    fix volunteer feature response

--- controllers/volunteerController.go
+++ controllers/volunteerController.go
@@ -12,62 +12,54 @@ import (
 func GetListVolunteersController(c echo.Context) error {
 	checkRole := c.Request().Header.Get("role")
 	if checkRole != "admin" {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Unauthorized access"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "unauthorized access"})
 	}
 
 	volunteer := []models.User{}
 	volunteers, rowAffected, err := libdb.ListVolunteers(&volunteer)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Failed to get list volunteers"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "failed to get list volunteers"})
 	}
 
 	if rowAffected == 0 {
-		return c.JSON(http.StatusOK, struct {
-			Status  string
-			Message string
-		}{Status: "Success", Message: "volunteer data not found"})
+		return c.JSON(http.StatusOK, models.ResponseOK{
+			Status:  "success",
+			Message: "volunteer data not found"})
 	}
 
-	return c.JSON(http.StatusOK, struct {
-		Status  string
-		Message string
-		Data    interface{}
-	}{Status: "Success", Message: "Success to get list volunteers", Data: volunteers})
+	return c.JSON(http.StatusOK, models.ResponseOK{
+		Status:  "success",
+		Message: "success to get list volunteers",
+		Data:    volunteers})
 }
 
 func GetVolunteerProfileController(c echo.Context) error {
 	volunteerId, errorId := strconv.Atoi(c.Request().Header.Get("userId"))
 	if errorId != nil {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Invalid volunteer id"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "invalid volunteer id"})
 	}
 
 	volunteer, rowAffected, err := libdb.GetVolunteerProfile(volunteerId)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Failed to get volunteer profile"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "failed to get volunteer profile"})
 	}
 
 	if rowAffected == 0 {
-		return c.JSON(http.StatusOK, struct {
-			Status  string
-			Message string
-		}{Status: "Success", Message: "volunteer data not found"})
+		return c.JSON(http.StatusOK, models.ResponseOK{
+			Status:  "success",
+			Message: "volunteer data not found"})
 	}
 
-	return c.JSON(http.StatusOK, struct {
-		Status  string
-		Message string
-		Data    interface{}
-	}{Status: "Success", Message: "Success to get volunteer profile", Data: volunteer})
+	return c.JSON(http.StatusOK, models.ResponseOK{
+		Status:  "success",
+		Message: "success to get volunteer profile",
+		Data:    volunteer})
 }
