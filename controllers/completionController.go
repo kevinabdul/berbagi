53527f58c3934bb0bd27dddea55cd51dadef4a2f
urnikrokhiyah <urnikrokhiyah123@gmail.com>
2021-10-12 19:15:37 +0700

    fix response in each feature

--- controllers/completionController.go
+++ controllers/completionController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	libdb "berbagi/lib/database"
+	models "berbagi/models"
 	"net/http"
 	"strconv"
 
@@ -11,103 +12,90 @@ import (
 func GetCompletionDetailController(c echo.Context) error {
 	checkRole := c.Request().Header.Get("role")
 	if checkRole != "admin" && checkRole != "volunteer" {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Unauthorized access"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "unauthorized access"})
 	}
 
 	volunteerId, _ := strconv.Atoi(c.Request().Header.Get("userId"))
 
 	verificationId, errorId := strconv.Atoi(c.Param("verificationId"))
 	if errorId != nil {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Invalid verification id"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "invalid verification id"})
 	}
 
 	completion, rowAffected, err := libdb.GetCompletionDetail(checkRole, verificationId, volunteerId)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Failed to get completion data"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "failed to get completion data"})
 	}
 
 	if rowAffected == 0 {
-		return c.JSON(http.StatusOK, struct {
-			Status  string
-			Message string
-		}{Status: "Success", Message: "verification id not found"})
+		return c.JSON(http.StatusOK, models.ResponseOK{
+			Status:  "success",
+			Message: "verification id not found"})
 	}
 
 	if rowAffected == -1 {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Unauthorize access"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "unauthorize access"})
 	}
 
-	return c.JSON(http.StatusOK, struct {
-		Status  string
-		Message string
-		Data    interface{}
-	}{Status: "Success", Message: "Success to get completion data", Data: completion})
+	return c.JSON(http.StatusOK, models.ResponseOK{
+		Status:  "success",
+		Message: "success to get completion data",
+		Data:    completion})
 
 }
 
 func UpdateStatusCompletionController(c echo.Context) error {
 	checkRole := c.Request().Header.Get("role")
 	if checkRole != "admin" {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Unauthorized access"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "unauthorized access"})
 	}
 
 	status := c.QueryParam("status")
 	if status != "verified" && status != "on-going" && status != "completed" {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Invalid completion status"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "invalid completion status"})
 	}
 
 	verificationId, errorId := strconv.Atoi(c.Param("verificationId"))
 	if errorId != nil {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Invalid verification id"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "invalid verification id"})
 	}
 
 	completion, rowAffected, err := libdb.UpdateCompletionStatus(status, verificationId)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Failed to update completion status"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "failed to update completion status"})
 	}
 
 	if rowAffected == 0 {
-		return c.JSON(http.StatusOK, struct {
-			Status  string
-			Message string
-		}{Status: "Success", Message: "verification id not found"})
+		return c.JSON(http.StatusOK, models.ResponseOK{
+			Status:  "success",
+			Message: "verification id not found"})
 	}
 
 	if rowAffected == -1 {
-		return c.JSON(http.StatusBadRequest, struct {
-			Status  string
-			Message string
-		}{Status: "Failed", Message: "Unauthorize access"})
+		return c.JSON(http.StatusBadRequest, models.ResponseNotOK{
+			Status:  "failed",
+			Message: "unauthorize access"})
 	}
 
-	return c.JSON(http.StatusOK, struct {
-		Status  string
-		Message string
-		Data    interface{}
-	}{Status: "Success", Message: "success to update completion status", Data: completion})
+	return c.JSON(http.StatusOK, models.ResponseOK{
+		Status:  "success",
+		Message: "success to update completion status",
+		Data:    completion})
 
 }
