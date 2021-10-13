ca21d4ae0dddb30dfe8a7159ddcad942d3ee71c0
Ilham Aris Hartadi <44136041+arieshta@users.noreply.github.com>
2021-10-13 16:13:50 +0700

    Add adjustments based on location tests

--- models/userModel.go
+++ models/userModel.go
@@ -11,7 +11,7 @@ import (
 type User struct {
 	ID        uint           `gorm:"primaryKey"`
 	Name      string         `gorm:"type:varchar(75)" json:"name"`
-	RoleID 	  uint		 	 `json:"role_id"`
+	RoleID    uint           `json:"role_id"`
 	NIK       string         `gorm:"unique; type:varchar(16)" json:"nik"`
 	Email     string         `gorm:"unique; type:varchar(50)" json:"email"`
 	Password  string         `json:"password"`
@@ -21,28 +21,31 @@ type User struct {
 }
 
 type LoginUserAPI struct {
-	Email    	string	`json:"email"`
-	Password 	string	`json:"password"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 type LoginSearchAPI struct {
-	ID      	uint    `json:id`
-	Email    	string	`json:"email"`
-	Password 	string	`json:"password"`
-	RoleID     	uint  	`json:"role_id"`
-	RoleName 	string  `json:"role_name"`
+	ID       uint   `json:id`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	RoleID   uint   `json:"role_id"`
+	RoleName string `json:"role_name"`
 }
 
 type LoginResponseAPI struct {
 	Message string `json:"message"`
-	Token   string	`json:"token"`
+	Token   string `json:"token"`
 }
 
 type UserProfile struct {
-	UserID uint
-	Name string
-	Role string
-	Address string
-	City string
-	Province string
+	UserID    uint    `json:"user_id"`
+	Name      string  `json:"name"`
+	Role      string  `json:"role"`
+	Address   string  `json:"address"`
+	City      string  `json:"city"`
+	Province  string  `json:"province"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Distance  float64 `json:"distance"`
 }
